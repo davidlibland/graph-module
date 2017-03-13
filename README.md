021e119d8c39b51b4137c18e7e45ce11d9f0df70
David Li-Bland <david.libland@gmail.com>
2017-03-13 02:56:30 -0700

    Added a writeup.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # graph-module
-This is a minimal graph module written in python. Full documentation can be found at https://davidlibland.github.io/graph-module/
+This is a minimal graph module written in python. Full documentation of the API can be found at https://davidlibland.github.io/graph-module/
--- graph/algorithms/connected_comp.py
+++ graph/algorithms/connected_comp.py
@@ -12,14 +12,17 @@ def connected_comp(g):
         :class:`graph.algorithms.GraphLabel`, or else behave like a dict. 
     
     """
+    # We initialize each node by setting node['cc'] to it's own id.
     def init_node(node):
         node['cc'] = node.name+" id:"+str(id(node))
         node['halt'] = True
     g.update_nodes(init_node) #Initialize vertices 
     
+    # Each edge sends messages to both nodes informing them of the value of each other's node['cc']
     def emitter(src,dst,e):
         return [dst['cc']],[src['cc']]
 
+    # The node sets node['cc'] to the smallest value received from it's neighbors.
     def collector(node,msg_iter):
         node['halt'] = True
         for near_cc in msg_iter:
